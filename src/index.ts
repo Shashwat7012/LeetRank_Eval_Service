9877786abc2d1bbd50e077300a2b9574d95f468f
Shashwat Pandey <pandeyshashwatptu@gmail.com>
2024-05-15 18:50:31 +0530

    added eslint confg.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import express, {Express} from "express";
+
+import express, { Express } from "express";
+
 import serverConfig from "./config/serverConfig";
+
+
+
 
 const app: Express = express();
 
-app.listen(serverConfig.PORT,()=>{
-    console.log(`server started at port  number *:${serverConfig.PORT}`);
-    console.log("wow");
-})
+app.listen(serverConfig.PORT, () => {
+  console.log(`server started at port  number *:${serverConfig.PORT}`);
+  console.log("wow");
+});
