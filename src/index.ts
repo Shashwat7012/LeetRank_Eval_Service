d0468999c657ee8ddf72a3dceff5b033a4bc8b72
Shashwat Pandey <pandeyshashwatptu@gmail.com>
2024-05-15 17:07:34 +0530

    basic TS + EXPRESS Project setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, {Express} from "express";
+import serverConfig from "./config/serverConfig";
+
+const app: Express = express();
+
+app.listen(serverConfig.PORT,()=>{
+    console.log(`server started at port  number *:${serverConfig.PORT}`);
+    console.log("wow");
+})
